0477e2b206627056760396a9c29cb35e350a83c4
ace <35115576+holt-ace@users.noreply.github.com>
2024-10-29 08:29:22 -0600

    edits

--- public/js/main.js
+++ public/js/main.js
@@ -193,7 +193,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "Buyer": "Nichole Bonin",
         "Order Date": "2024-09-23",
         "Delivery Date": "2024-10-28",
-        "Status": "TRANSIT",
+        "Status": "DELIVERED",
         "Invoice": "",
         "Type": "SHELF STABLE",
         "pdf": "https://github.com/holt-ace/po-dashboard/blob/master/Sysco%20_PO_Confirmation/PO_27312200.pdf"

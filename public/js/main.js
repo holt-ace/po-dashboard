ea2ab08c615100198546ceef78fec20d805217b2
ace <35115576+holt-ace@users.noreply.github.com>
2024-11-19 19:59:58 -0700

    delivery

--- public/js/main.js
+++ public/js/main.js
@@ -237,8 +237,8 @@ document.addEventListener('DOMContentLoaded', function() {
         "Buyer": "Kinsey Collins",
         "Order Date": "2024-10-25",
         "Delivery Date": "2024-11-19",
-        "Status": "CONFIRMED",
-        "Invoice": "",
+        "Status": "DELIVERED",
+        "Invoice": "SENT",
         "Type": "SHELF STABLE",
         "pdf": "https://github.com/holt-ace/po-dashboard/blob/master/Sysco%20_PO_Confirmation/PO_30765210.pdf"
     },

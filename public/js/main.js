98867d038718906b34b750c87883375b41c2a823
ace <35115576+holt-ace@users.noreply.github.com>
2024-12-06 08:09:15 -0700

    Chicago Farro

--- public/js/main.js
+++ public/js/main.js
@@ -270,11 +270,23 @@ document.addEventListener('DOMContentLoaded', function() {
         "Buyer": "Nadia Navarro",
         "Order Date": "2024-11-11",
         "Delivery Date": "2024-11-29",
-        "Status": "CONFIRMED",
+        "Status": "DELIVERED",
         "Invoice": "SENT",
         "Invoice #": "#1025",
         "Type": "SHELF STABLE",
         "pdf": "https://github.com/holt-ace/po-dashboard/blob/master/Sysco%20_PO_Confirmation/PO_27505230.pdf"
+    },
+    {
+        "Purchase Order": "PO-29924220",
+        "Customer": "Sysco Chicago",
+        "Buyer": "Aaron Reichow",
+        "Order Date": "2024-12-05",
+        "Delivery Date": "2024-12-31",
+        "Status": "CONFIRMED",
+        "Invoice": "",
+        "Invoice #": "",
+        "Type": "SHELF STABLE",
+        "pdf": "https://github.com/holt-ace/po-dashboard/blob/master/Sysco%20_PO_Confirmation/PO_29924220.pdf"
     }
 ];
 
